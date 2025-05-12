a4c645bf61f05b8a6452473e55087d07703013d6
Samuel Useche Chaparro <samuuseche01@gmail.com>
2025-05-12 16:04:16 -0500

    feat: Add router

---
+++ App/router.php
@@ -0,0 +1,30 @@
+<?php
+class Router
+{
+    private $controller;
+    private $method;
+
+    public function __construct()
+    {
+        $this->machtRoute();
+    }
+
+    public function machtRoute()
+    {
+        $url = explode("/", URL);
+        $this->controller = !empty($url[1]) ? $url[1] : "Page";
+        $this->method = !empty($url[2]) ? $url[2] : "home";
+
+        $this->controller = $this->controller . "Controller";
+        require_once(__DIR__ . "/Controllers/" . $this->controller . ".php");
+    }
+
+    public function run()
+    {
+        $database = new Database();
+        $connection = $database->getConnection();
+        $controller = new $this->controller($connection);
+        $method = $this->method;
+        $controller->$method();
+    }
+}
